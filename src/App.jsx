6bdb8e51e25d79e4c8f3c1a6789cd29686427e8a
Júlio Costaf <juliocesarcostaf6@gmail.com>
2025-02-25 19:22:45 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+const login = () => {
+  return (
+    <div className="entrar">
+      <h3>Login</h3>
+      <form className="entrarForm">
+        <div className="inputGroup">
+          <input
+            type="text"
+            id="email"
+            autoComplete="off"
+            placeholder="email ou usuário"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+function App() {
+  <>
+    <h3></h3>
+  </>;
+}
+
+export default App;
